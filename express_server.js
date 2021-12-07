c08beeba4c3809ca6dcef4d61fa7b0c1619036b0
Jovita <jovitaspais@gmail.com>
2021-12-07 19:54:47 +0000

    Update your express server so that when it receives a POST request, it responds with a redirection:URL Shortening (Part 2)

--- express_server.js
+++ express_server.js
@@ -10,20 +10,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 
 //Routes
 
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -33,26 +33,50 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  // console.log(req.body);   // Log the POST request body to the console
+  const shortURL = req.params.shortURL; //keys
+  const longURL = urlDatabase[shortURL];  //values
   const templateVars = { shortURL: shortURL, longURL: longURL };
   res.render("urls_show", templateVars);
+  // let shortURLGenerated = generateRandomString();
+  // console.log(shortURLGenerated);
+  // res.redirect(`/urls/${shortURLGenerated}`);
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  // console.log(longURL);
+  res.redirect(longURL);
+});
+  
 app.get("/urls.json", (req, res) => { //adding additional endpoints
   res.json(urlDatabase);
 });
 
 //POST routes
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.post("/urls/", (req, res) => {
+  const longURL = req.body.longURL;
+  let shortURLGenerated = generateRandomString();
+  console.log(shortURLGenerated);
+  urlDatabase[shortURLGenerated] = longURL;
+  //const templateVars = { shortURL, longURL };
+  //console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/${shortURLGenerated}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-const generateRandomString = function() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-
-};
+// eslint-disable-next-line func-style
+function generateRandomString() {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 6; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+// /*const generateRandomString = function() {
+//   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(0,11);
+  
+// };*/
+// let string = generateRandomString();
+// console.log("**%&46", string);
