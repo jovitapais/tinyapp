e5821f74a66949d1f8f29af7d03fff15621a6acc
Jovita <jovitaspais@gmail.com>
2021-12-06 20:26:19 -0500

    created new files due to github bug

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +21,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_show", templateVars);
+});
 app.get("/urls.json", (req, res) => { //adding additional endpoints
   res.json(urlDatabase);
 });
